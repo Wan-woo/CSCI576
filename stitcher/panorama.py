8b1d0a50d3eeb40b9d51292f86205c53b66f4575
Wanyu Z <wanyuzha@usc.edu>
2022-12-06 17:09:05 -0800

    可用的结果

--- stitcher/panorama.py
+++ stitcher/panorama.py
@@ -10,17 +10,97 @@ class Stitcher:
         self.count = len(images)
         self.left_list = []
         self.right_list = []
+        self.Hmatrixs = []
+        self.masks = []
         self.prepare_lists()
 
     def prepare_lists(self):
         self.centerIdx = self.count/2 
         self.center_im = self.images[int(self.centerIdx)]
         for i in range(self.count):
+            # Applying Cylindrical projection on Image
+            Image_Cyl, mask_x, mask_y = self.ProjectOntoCylinder(self.images[i])
+
+            # Getting Image Mask
+            Image_Mask = np.zeros(Image_Cyl.shape, dtype=np.uint8)
+            Image_Mask[mask_y, mask_x, :] = 255
+            self.images[i] = Image_Cyl
+            self.masks.append(Image_Mask)
+            self.images[i] = cv2.resize(self.images[i],None,fx=0.5, fy=0.5, interpolation = cv2.INTER_CUBIC)
             if(i<=self.centerIdx):
                 self.left_list.append(self.images[i])
             else:
                 self.right_list.append(self.images[i])
         print("Image lists prepared")
+
+    def Convert_xy(self, x, y):
+        global center, f
+
+        xt = ( f * np.tan( (x - center[0]) / f ) ) + center[0]
+        yt = ( (y - center[1]) / np.cos( (x - center[0]) / f ) ) + center[1]
+        
+        return xt, yt
+
+
+    def ProjectOntoCylinder(self, InitialImage):
+        global w, h, center, f
+        h, w = InitialImage.shape[:2]
+        center = [w // 2, h // 2]
+        f = 1100       # 1100 field; 1000 Sun; 1500 Rainier; 1050 Helens
+        
+        # Creating a blank transformed image
+        TransformedImage = np.zeros(InitialImage.shape, dtype=np.uint8)
+        
+        # Storing all coordinates of the transformed image in 2 arrays (x and y coordinates)
+        AllCoordinates_of_ti =  np.array([np.array([i, j]) for i in range(w) for j in range(h)])
+        ti_x = AllCoordinates_of_ti[:, 0]
+        ti_y = AllCoordinates_of_ti[:, 1]
+        
+        # Finding corresponding coordinates of the transformed image in the initial image
+        ii_x, ii_y = self.Convert_xy(ti_x, ti_y)
+
+        # Rounding off the coordinate values to get exact pixel values (top-left corner)
+        ii_tl_x = ii_x.astype(int)
+        ii_tl_y = ii_y.astype(int)
+
+        # Finding transformed image points whose corresponding 
+        # initial image points lies inside the initial image
+        GoodIndices = (ii_tl_x >= 0) * (ii_tl_x <= (w-2)) * \
+                    (ii_tl_y >= 0) * (ii_tl_y <= (h-2))
+
+        # Removing all the outside points from everywhere
+        ti_x = ti_x[GoodIndices]
+        ti_y = ti_y[GoodIndices]
+        
+        ii_x = ii_x[GoodIndices]
+        ii_y = ii_y[GoodIndices]
+
+        ii_tl_x = ii_tl_x[GoodIndices]
+        ii_tl_y = ii_tl_y[GoodIndices]
+
+        # Bilinear interpolation
+        dx = ii_x - ii_tl_x
+        dy = ii_y - ii_tl_y
+
+        weight_tl = (1.0 - dx) * (1.0 - dy)
+        weight_tr = (dx)       * (1.0 - dy)
+        weight_bl = (1.0 - dx) * (dy)
+        weight_br = (dx)       * (dy)
+        
+        TransformedImage[ti_y, ti_x, :] = ( weight_tl[:, None] * InitialImage[ii_tl_y,     ii_tl_x,     :] ) + \
+                                        ( weight_tr[:, None] * InitialImage[ii_tl_y,     ii_tl_x + 1, :] ) + \
+                                        ( weight_bl[:, None] * InitialImage[ii_tl_y + 1, ii_tl_x,     :] ) + \
+                                        ( weight_br[:, None] * InitialImage[ii_tl_y + 1, ii_tl_x + 1, :] )
+
+
+        # Getting x coorinate to remove black region from right and left in the transformed image
+        min_x = min(ti_x)
+
+        # Cropping out the black region from both sides (using symmetricity)
+        TransformedImage = TransformedImage[:, min_x : -min_x, :]
+
+        return TransformedImage, ti_x-min_x, ti_y
+
 
     def leftshift(self):
 		# self.left_list = reversed(self.left_list)
@@ -45,7 +125,7 @@ class Stitcher:
             print("Homography is : ", H)
             xh = np.linalg.inv(H)
             print("Inverse Homography :", xh)
-            ds = np.dot(xh, np.array([a.shape[1], a.shape[0], 1]));
+            ds = np.dot(xh, np.array([a.shape[1], a.shape[0], 1]))
             ds = ds/ds[-1]
             print("final ds=>", ds)
             f1 = np.dot(xh, np.array([0,0,1]))
@@ -53,24 +133,32 @@ class Stitcher:
             xh[0][-1] += abs(f1[0])
             xh[1][-1] += abs(f1[1])
             ds = np.dot(xh, np.array([a.shape[1], a.shape[0], 1]))
+            #ds = ds/ds[-1]
             offsety = abs(int(f1[1]))
             offsetx = abs(int(f1[0]))
             dsize = (int(ds[0])+offsetx, int(ds[1]) + offsety)
             print("image dsize =>", dsize)
-            #dsize = ((imageA.shape[1] + imageB.shape[1], imageA.shape[0]))
+            print("entering warpPerspective")
             tmp = cv2.warpPerspective(a, xh, dsize)
-            cv2.imshow("warped", tmp)
-            cv2.waitKey()
+            print("done warpPerspective")
             tmp[offsety:b.shape[0]+offsety, offsetx:b.shape[1]+offsetx] = b
+            #ixh = np.linalg.inv(xh)
+            #max_length = b.shape[1]+offsetx
+            #max_width = int(np.dot(ixh, np.array([0,1080,1]))[1])+offsety
+            #tmp = tmp[0:max_width, 0:max_length]
+            #cv2.imshow("warped", tmp)
+            #cv2.waitKey()
             a = tmp
         self.leftImage = tmp
 
 		
     def rightshift(self):
         for each in self.right_list:
+            print("enter right shift")
             (imageB, imageA) = (self.leftImage, each)
             (kpsA, featuresA) = self.detectAndDescribe(imageA)
             (kpsB, featuresB) = self.detectAndDescribe(imageB)
+            print("detection is done")
             # match features between the two images
             M = self.matchKeypoints(kpsA, kpsB, featuresA, featuresB, ratio=0.75, reprojThresh=4.0)
             # if the match is None, then there aren’t enough matched
@@ -86,8 +174,8 @@ class Stitcher:
             txyz = txyz/txyz[-1]
             dsize = (int(txyz[0])+self.leftImage.shape[1], int(txyz[1])+self.leftImage.shape[0])
             tmp = cv2.warpPerspective(each, H, dsize)
-            cv2.imshow("tp", tmp)
-            cv2.waitKey()
+            #cv2.imshow("tp", tmp)
+            #cv2.waitKey()
 			# tmp[:self.leftImage.shape[0], :self.leftImage.shape[1]]=self.leftImage
             tmp = self.mix_and_match(self.leftImage, tmp)
             print("tmp shape",tmp.shape)
@@ -102,12 +190,13 @@ class Stitcher:
         i2y, i2x = warpedImage.shape[:2]
         print(leftImage[-1,-1])
 
+        print("mix and matching")
         t = time.time()
         black_l = np.where(leftImage == np.array([0,0,0]))
         black_wi = np.where(warpedImage == np.array([0,0,0]))
         print(time.time() - t)
         print(black_l[-1])
-
+        '''
         for i in range(0, i1x):
             for j in range(0, i1y):
                 try:
@@ -132,7 +221,70 @@ class Stitcher:
                     pass
 		# cv2.imshow("waRPED mix", warpedImage)
 		# cv2.waitKey()
+        '''
         return warpedImage
+    
+    def rightHmatrix(self):
+        pass
+
+    def computeHmatrix(self):
+        # self.left_list = reversed(self.left_list)
+        a = self.images[0]
+        i = 0
+        tmp = a
+        historyOffsetX = 0
+        historyOffsetY = 0
+        for b in self.images[1:]:
+            i += 1
+            print("running list is %d", i)
+            (imageB, imageA) = (a, b)
+            (kpsA, featuresA) = self.detectAndDescribe(imageA)
+            (kpsB, featuresB) = self.detectAndDescribe(imageB)
+            # match features between the two images
+            M = self.matchKeypoints(kpsA, kpsB, featuresA, featuresB, ratio=0.75, reprojThresh=2.0)
+            # if the match is None, then there aren’t enough matched
+            # keypoints to create a panorama
+            if M is None:
+                return None
+
+            # otherwise, apply a perspective warp to stitch the images
+            # together
+            (matches, H, status) = M
+            print("Homography is : ", H)
+            xh = np.linalg.inv(H)
+            print("Inverse Homography :", xh)
+            ds = np.dot(xh, np.array([a.shape[1], a.shape[0], 1]))
+            ds = ds/ds[-1]
+            print("final ds=>", ds)
+            f1 = np.dot(xh, np.array([0,0,1]))
+            f1 = f1/f1[-1]
+            xh[0][-1] += abs(f1[0])
+            xh[1][-1] += abs(f1[1])
+            ds = np.dot(xh, np.array([tmp.shape[1], tmp.shape[0], 1]))
+            #ds = ds/ds[-1]
+            offsety = abs(int(f1[1]))
+            offsetx = abs(int(f1[0]))
+            dsize = (int(ds[0])+offsetx, int(ds[1]) + offsety)
+            historyOffsetX += offsetx
+            historyOffsetY += offsety
+            self.Hmatrixs.append(xh)
+            print("image dsize =>", dsize)
+            print("entering warpPerspective")
+            tmp = cv2.warpPerspective(tmp, xh, dsize)
+            tmp[0:dsize[0], historyOffsetX:dsize[1]] = [0,0,0]
+            tmp[historyOffsetY:b.shape[0]+historyOffsetY, historyOffsetX:b.shape[1]+historyOffsetX] = b
+            #cylinder = tmp[historyOffsetY:b.shape[0]+historyOffsetY, historyOffsetX:b.shape[1]+historyOffsetX]
+            #tmp[historyOffsetY:b.shape[0]+historyOffsetY, historyOffsetX:b.shape[1]+historyOffsetX] = cv2.bitwise_or(b, cv2.bitwise_and(cylinder, cv2.bitwise_not(self.masks[i])))
+            print("done warpPerspective")
+            #ixh = np.linalg.inv(xh)
+            #max_length = b.shape[1]+offsetx
+            #max_width = int(np.dot(ixh, np.array([0,1080,1]))[1])+offsety
+            #tmp = tmp[0:max_width, 0:max_length]
+            #cv2.imshow("warped", tmp)
+            #cv2.waitKey()
+            a = b
+        self.leftImage = tmp
+
 
     def stitch(self, images, ratio=0.75, reprojThresh=4.0,showMatches=False):
         # unpack the images, then detect keypoints and extract
@@ -178,8 +330,10 @@ class Stitcher:
         # check to see if we are using OpenCV 3.X
         if self.isv3:
             # detect and extract features from the image
+            print("start detecting")
             descriptor = cv2.xfeatures2d.SIFT_create()
             (kps, features) = descriptor.detectAndCompute(image, None)
+            print("detect done")
             # otherwise, we are using OpenCV 2.4.X
         else:
             # detect keypoints in the image
--- stitcher/test.py
+++ stitcher/test.py
@@ -14,12 +14,26 @@ image_list = []
 
 image_list.append(cv2.imread("images/frame_345.jpg"))
 image_list.append(cv2.imread("images/frame_300.jpg"))
+image_list.append(cv2.imread("images/frame_275.jpg"))
+image_list.append(cv2.imread("images/frame_265.jpg"))
+image_list.append(cv2.imread("images/frame_260.jpg"))
+image_list.append(cv2.imread("images/frame_255.jpg"))
+
 image_list.append(cv2.imread("images/frame_250.jpg"))
+image_list.append(cv2.imread("images/frame_235.jpg"))
+image_list.append(cv2.imread("images/frame_225.jpg"))
+
 
 image_list.append(cv2.imread("images/frame_200.jpg"))
+image_list.append(cv2.imread("images/frame_175.jpg"))
 image_list.append(cv2.imread("images/frame_150.jpg"))
+image_list.append(cv2.imread("images/frame_125.jpg"))
 image_list.append(cv2.imread("images/frame_100.jpg"))
-image_list.append(cv2.imread("images/frame_20.jpg"))
+
+#image_list.append(cv2.imread("images/frame_75.jpg"))
+#image_list.append(cv2.imread("images/frame_50.jpg"))
+#image_list.append(cv2.imread("images/frame_20.jpg"))
+''''''
 '''
 image_list.append(cv2.imread("images/S1.jpg"))
 image_list.append(cv2.imread("images/S2.jpg"))
@@ -28,8 +42,23 @@ image_list.append(cv2.imread("images/S3.jpg"))
 image_list.append(cv2.imread("images/S5.jpg"))
 image_list.append(cv2.imread("images/S6.jpg"))
 '''
+
+
+
 # stitch the images together to create a panorama
 stitcher = Stitcher(image_list)
+'''
+result1 = stitcher.stitch([image_list[0], image_list[1]], showMatches=False)
+result2 = stitcher.stitch([image_list[2], image_list[3]], showMatches=False)
+result3 = stitcher.stitch([image_list[4], image_list[5]], showMatches=False)
+result4 = stitcher.stitch([image_list[6], image_list[7]], showMatches=False)
+result5 = stitcher.stitch([image_list[8], image_list[9]], showMatches=False)
+result6 = stitcher.stitch([image_list[10], image_list[11]], showMatches=False)
+
+image_list.clear()
+image_list = [result1, result2, result3, result4, result5, result6]
+stitcher.__init__(image_list)
+'''
 '''
 result = stitcher.stitch([image_list[0], image_list[1]], showMatches=False)
 for i in range(2, len(image_list)):
@@ -42,12 +71,14 @@ result = stitcher.stitch([resultA, resultB], showMatches=False)
 '''
 #(result, vis) = stitcher.stitch([imageA, imageB], showMatches=True)
 
-stitcher.leftshift()
-stitcher.rightshift()
+#stitcher.leftshift()
+#stitcher.rightshift()
+stitcher.computeHmatrix()
 
 # show the images
 #cv2.imshow("Image A", imageA)
 #cv2.imshow("Image B", imageB)
 #cv2.imshow("Keypoint Matches", vis)
-cv2.imshow("Result", stitcher.leftImage)
-cv2.waitKey(0)
+#cv2.imshow("Result", stitcher.leftImage)
+#cv2.waitKey(0)
+cv2.imwrite("Stitched_Panorama.png", stitcher.leftImage)
